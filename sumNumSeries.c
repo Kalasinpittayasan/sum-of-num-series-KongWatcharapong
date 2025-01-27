268da6af14df22ff59fea22c1f58d4765a248662
Watcharapong Namsaeng (KPS E-MAIL) <47651@kalasinpit.ac.th>
2025-01-27 15:10:12 +0700

    Update sumNumSeries.c

--- sumNumSeries.c
+++ sumNumSeries.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 int main() {
-    //ระบุ n
-    int n = 50;
+    int n;
     scanf("%d", &n);
 
-    //คำนวณ
     int i = 0, sum = 0;
     while (i < n) {
         i++;
         sum += i;
     }
-
-    //คำตอบ
     printf("%d", sum);
 }
